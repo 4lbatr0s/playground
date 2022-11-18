e866fa925f15b75745553de21dd129694f42f200
serhat0ner <serhatoner@protonmail.com>
2022-11-19 01:56:28 +0300

    created admin dashboard project, created topbar and sidebar components

---
+++ react-admin-dashboard-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Sidebar from './components/sidebar/Sidebar'
+import Topbar from './components/topbar/Topbar'
+
+function App() {
+
+  return (
+    <div>
+      <Topbar />
+      <div className="container">
+        <Sidebar/>
+        <div className="others">Others</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
