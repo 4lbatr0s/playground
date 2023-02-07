839545699a01372001d908f22c803ef0fc7d41d7
4lbatr0s <serhatoner@protonmail.com>
2023-02-07 18:06:36 +0300

    INITIAL

--- JSConfusion/part-one.js
+++ JSConfusion/part-one.js
@@ -55,11 +55,51 @@ console.log(student);//No eror no change
 
 
 
-//VAR Keywords: functional and global  scopes.
-//Works like the ref keyword in the C#.
+//INFO:VAR Keywords: functional and global  scopes.
+//INFO:Works like the ref keyword in the C#.
+console.log("var keyword area")
 var x =1;
 if(x===1){
-    var x = 10; //It changes!
+    var x = 10; //It changes because var is global scope!
     console.log(x);
 }
 console.log(x);
+
+console.log("let keyword area")
+//LET Keyword: block scope! if,else else if etc.
+let z = 1;
+console.log(z);
+if(z === 1)
+{
+    let z = 10; //does not change because this z iz special to the block scope!
+    console.log(z);
+}
+console.log(z);
+
+//INFO:USE CASES
+console.log("Use cases of VAR/LET/CONST:");
+var test = 10; //We can reach it with window property.
+console.log(window.x);
+let testlet = 20;
+console.log(window.testlet); //undefined
+
+//INFO:HOISTING:
+console.log("----------------------------------------------------------------");
+console.log("HOISTING:");
+console.log(bet); //undefined
+var bet=10;
+
+// console.log(betlet); //INFO: Hoisting does not work for LET! REFERENCE ERROR!
+// let betlet = 20;
+
+// console.log(betconst);//INFO: Hoisting does not work for CONST! REFERENCE ERROR!
+// const betconst = 30;
+
+console.log("Hoisting for functions:");
+//INFO: Hoisting works for functions completely! so no undefined at all!
+callHelloWorld();
+
+function callHelloWorld() {
+    let helloWorld = "Hello World!";
+    console.log(helloWorld);
+}
---
+++ JSConfusion/part-two.js
@@ -0,0 +1,124 @@
+//INFO: This module will contain  Fonksiyonlar, Declaration vs Expression, IIFE, First-Class Fonksiyonlar
+
+// Function Declaration:
+console.log(
+    '---------------Function Declaration--------------------------------'
+);
+console.log(square(10)); //TIP: Works with HOISTING!
+function square(num) {
+    return num * num;
+}
+square(5);
+console.log(square(5));
+console.log(square); //TIP:Returns function information.
+console.log(square()); //NaN
+
+console.log(
+    '---------------Function Expression--------------------------------'
+);
+//TIP: NOT HOISTING! REFERENCE ERROR! BECAUSE ITS ASSIGNED TO VARIABLES (VAR, CONST, LET) THEREFORE ITS NOT A FUNCTION ITS A VARIABLE NOW!
+// console.log(sum(10,20));
+//TIP: Anonymous functions!
+const sum = function (a, b) {
+    return a + b;
+};
+console.log(sum);
+console.log(sum()); //NaN
+
+//INFO:In javascript, FUNCTIONS ARE VARIABLES! THEY'RE FIRST CLASS FUNCTIONS!
+const myArr = [
+    6,
+    'Arin',
+    function () {
+        console.log('Array Element!');
+    },
+];
+myArr[2]();
+
+const myObj = {
+    age: 5,
+    name: 'Serhat',
+    func: function () {
+        console.log('Object element!');
+    },
+};
+console.log(myObj.func());
+
+console.log(
+    20 +
+        function () {
+            return 10;
+        }
+); //TIP: returns 20function(){return 10;} because we didn't invoke the func!
+//INFO:IIFE: Immediately invoked function expression
+console.log(
+    20 +
+        (function () {
+            return 10;
+        })()
+); //TIP: returns 30, we invoked the function!
+
+console.log('---------------Function Expression vs Declaration--------------------------------');
+//INFO:FUNCTION EXPRESSION VS DECLARATION
+/**
+ * 1.Function Expression is not HOISTED
+ * 2.Function Expression can be anonymous
+ * 3.FIRST CLASS FUNCTIONS can be passed as a arguments to another function.
+ * 4. FIRST CLASS FUNCTIONS CAN be used as a second function in the return of another function!
+ */
+
+//INFO: * 3.FIRST CLASS FUNCTIONS can be passed as a arguments to another function.
+const addFive = function (num, func) {
+    console.log(num + func());
+};
+
+console.log(
+    addFive(10, function () {
+        return 50;
+    })
+); //RETURNS 60
+
+console.log('---------------FIRST CLASS FUNCTIONS CAN BE USED AS A RETURN VALUE!--------------------------------');
+//INFO:4. FIRST CLASS FUNCTIONS CAN be used as a second function in the return of another function!
+const myFunc = function (num) {
+    return function toDouble() {
+        console.log(num * 2);
+    };
+};
+
+myFunc(8)();//16!
+//TIP:Or..
+
+
+console.log('---------------IIFE IN DECLARATION VS EXPRESSION!--------------------------------');
+/**
+ * IIFE: Immediately Invoked Function Expression
+*/
+
+//Expression:
+const calc = function(){
+    console.log(12+5);
+}() //OUTPUTS 17
+console.log(calc);// OUTPUTS Undefined
+
+
+//Declaration:
+//we have to use this paranthesis because when we write function, javascript thinks we're going to use declaration 
+//but we don't because its an anonymous function!
+(function(){
+    console.log(12+5);
+})();
+
+//INFO: FUNCTIONS ARE OBJECTS IN JAVASCRIPT!
+
+
+
+//INFO:SUMMARY:
+/**
+ * 1.Function Expression is not HOISTED
+ * 2.Function Expression can be anonymous
+ * 3.FIRST CLASS FUNCTIONS can be passed as a arguments to another function.
+ * 4. FIRST CLASS FUNCTIONS CAN be used as a second function in the return of another function!
+ * 5.FUNCTIONS IN JS ARE OBJECTS!
+ * 6.WE SHOULD USE AN EXTRA () IN THE DECLARATION WHEN WE USE IIFE
+ */
